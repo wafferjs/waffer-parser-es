fb5805e760ef7a5776d748a699215bd2d0a32919
wvffle <casper@wvffle.net>
2018-03-30 11:17:00 +0200

    babel -> browserify + babelify

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const babel = require('babel-core')
-
-const parse = async (file, exporting = false, options = {}) => {
-  return new Promise((resolve, reject) => {
-    babel.transformFile(file, options, (err, res) => {
-      if (err) {
-        return reject(err)
-      }
-
-      return resolve({ content: res.code })
-    })
-  })
-}
+const browserify = require('browserify')
 
 module.exports = server => {
+  const { debug, prod } = server.options
+  const parse = async (file, exporting = false, options = {}) => {
+    return new Promise((resolve, reject) => {
+      const b = browserify(file)
+        .transform(`${__dirname}/node_modules/babelify`, {
+          presets: [ `${__dirname}/node_modules/babel-preset-env`, {} ],
+          sourceMaps: debug ? 'inline' : false,
+          minified: prod,
+        }).bundle()
+
+      let js = ''
+      b.on('data', chunk => (js += chunk))
+      b.on('end', _ => resolve({ content: js }))
+      b.on('error', err => reject(err))
+    })
+  }
+
   return { parse, ext: '.js' }
 }
