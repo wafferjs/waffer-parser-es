0bfe78c4f83e924135dc5729885f08ac4aac4f76
wvffle <casper@wvffle.net>
2018-03-30 11:58:49 +0200

    added vue support

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const browserify = require('browserify')
+const babelify = require('babelify')
+const vueify = require('vueify')
 
 module.exports = server => {
   const { debug, prod } = server.options
   const parse = async (file, exporting = false, options = {}) => {
     return new Promise((resolve, reject) => {
-      const b = browserify(file)
-        .transform(`${__dirname}/node_modules/babelify`, {
+      vueify.compiler.applyConfig({
+        customCompilers: Object.keys(server.parsers).map(ext => {
+          const parser = (str, next, instance, file) => {
+            const promise = server.parser(ext).parse(str, false, { fragment: true })
+            promise.then(content => next(null, content))
+            promise.catch(err => next(err))
+          }
+
+          if (ext[0] === '.') ext = ext.slice(1)
+
+          return { ext, parser }
+        }).reduce((a, b) => {
+          a[b.ext] = b.parser
+          return a
+        }, {}),
+      })
+
+      const b = browserify(file, { debug })
+        .transform(babelify, {
           presets: [ `${__dirname}/node_modules/babel-preset-env`, {} ],
-          sourceMaps: debug ? 'inline' : false,
+          sourceMaps: 'inline',
           minified: prod,
-        }).bundle()
+        })
+        .transform(vueify)
+        .bundle()
 
       let js = ''
       b.on('data', chunk => (js += chunk))
       b.on('end', _ => resolve({ content: js }))
-      b.on('error', err => reject(err))
+      b.on('error', err => reject(new Error(`${err}`)))
     })
   }
 
